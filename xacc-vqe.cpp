2178a7c58e3e65c26f9ad0f5f8bd43aad114fa99
Alex McCaskey <mccaskeyaj@ornl.gov>
2017-11-08 02:00:01 +0000

    minor cleanup
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- xacc-vqe.cpp
+++ xacc-vqe.cpp
@@ -1,4 +1,5 @@
 #include "XACC.hpp"
+#include "ServiceRegistry.hpp"
 #include "VQEProgram.hpp"
 #include "VQETask.hpp"
 #include "VQEParameterGenerator.hpp"
@@ -10,7 +11,7 @@ int main(int argc, char** argv) {
 	// All our important stuff is in the xacc::vqe namespace
 	using namespace xacc::vqe;
 
-	//VQETaskFactory factory;
+	// Initialize MPI, create a VQEProgram pointer
 	std::shared_ptr<VQEProgram> program;
 	mpi::environment env(argc, argv);
 	mpi::communicator world;
@@ -18,10 +19,12 @@ int main(int argc, char** argv) {
 	// Set the default Accelerator to TNQVM
 	xacc::setAccelerator("tnqvm");
 
+	// Add some command line options for this XACC app
 	xacc::addCommandLineOptions("XACC VQE", std::map<std::string, std::string>{
-		{"vqe-kernel-file", "(required) The file containing an XACC Kernel describing "
-			"a molecular fermionic Hamiltonian."},
+		{"vqe-kernel-file", "(required) The file containing XACC Kernels describing "
+			"a Hamiltonian."},
 		{"vqe-task", "(required)"},
+		{"vqe-list-tasks", "(optional) List all available VQE Tasks."},
 		{"vqe-state-prep-kernel", "(optional) Provide the file name of the state "
 				"preparation circuit."},
 		{"vqe-state-prep-kernel-compiler", "(optional) If not scaffold, provide the "
@@ -33,19 +36,37 @@ int main(int argc, char** argv) {
 						"convergence of Nelder-Mead minimization."}
 	});
 
+	// Initialize the framework
 	xacc::Initialize(argc, argv);
 
+	if (xacc::optionExists("vqe-list-tasks")) {
+		XACCInfo("");
+		auto allTasks = xacc::ServiceRegistry::instance()->getServices<VQETask>();
+		for (auto a : allTasks) {
+			XACCInfo("VQE Task: " + a->name());
+		}
+		xacc::Finalize();
+		exit(0);
+	}
+
+	// Users must specify a task
 	if (!xacc::optionExists("vqe-task")) {
 		XACCError("You must specify a vqe task to run.");
 	}
 
+	// Users must specify a file containing VQE Hamiltonian kernels
 	if (!xacc::optionExists("vqe-kernel-file")) {
 		XACCError("You must at least specify a kernel file to run this app.");
 	}
 
+	// Get the user-specified Accelerator,
+	// or TNQVM if none specified
 	auto accelerator = xacc::getAccelerator();
+
+	// Get the task to run
 	auto task = xacc::getOption("vqe-task");
 
+	// Read in the Hamiltonian kernel vile
 	std::ifstream moleculeKernelHpp(xacc::getOption("vqe-kernel-file"));
 	std::string src((std::istreambuf_iterator<char>(moleculeKernelHpp)),
 			std::istreambuf_iterator<char>());
