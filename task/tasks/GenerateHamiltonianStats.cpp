f219cabe8162e6f482e1697d20b892a54d6e10f1
Alex McCaskey <mccaskeyaj@ornl.gov>
2017-12-01 18:31:44 +0000

    outputting gen stats to file
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- task/tasks/GenerateHamiltonianStats.cpp
+++ task/tasks/GenerateHamiltonianStats.cpp
@@ -17,6 +17,16 @@ VQETaskResult GenerateHamiltonianStats::execute(
 	auto nParameters = program->getNParameters();
 
 	if (comm.rank() == 0) {
+		std::ofstream out("gen_hamiltonian_stats_out.txt");
+
+		std::stringstream s;
+		s << "Number of Qubits = " << xacc::getOption("n-qubits") << "\n";
+		s << "Number of Hamiltonian Terms = "
+				<< std::to_string(kernels.size()) << "\n";
+		s << "State Prep Type: " << statePrepType << "\n";
+		s << "Number of Variational Parameters = "
+				<< std::to_string(nParameters) << "\n";
+
 		XACCInfo("Number of Qubits = " + xacc::getOption("n-qubits"));
 		XACCInfo(
 				"Number of Hamiltonian Terms = "
@@ -46,7 +56,13 @@ VQETaskResult GenerateHamiltonianStats::execute(
 			XACCInfo(
 					"N k-Local Terms (k,N) = (" + std::to_string(it.first)
 							+ ", " + std::to_string(it.second) + ")");
+			s << "N k-Local Terms (k,N) = (" << std::to_string(it.first)
+			<< ", " << std::to_string(it.second) << ")" << "\n";
 		}
+
+		out << s.str();
+		out.flush();
+		out.close();
 	}
 
 	return VQETaskResult{};
