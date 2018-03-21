6ba0702a2eaced4dfe09da16f1a8cf3bc0d553e2
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-03-21 18:40:21 +0000

    adding comments to pso code
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- examples/parallel_pso/pso_vqe.py
+++ examples/parallel_pso/pso_vqe.py
@@ -4,10 +4,13 @@ import numpy as np
 from stochopy import Evolutionary
 from mpi4py import MPI
 
+# Initialize MPI and XACC
 mpi_comm = MPI.COMM_WORLD
 mpi_rank = mpi_comm.Get_rank()
+xacc.Initialize()
+xacc.setOption('n-electrons', '2')
 
-src = """&FCI NORB=  2,NELEC=  2,MS2= 0,
+op = vqe.compile("""&FCI NORB=  2,NELEC=  2,MS2= 0,
   ORBSYM=1,5,
   ISYM=0,
  /                         i   a   j   b
@@ -18,20 +21,23 @@ src = """&FCI NORB=  2,NELEC=  2,MS2= 0,
  -0.1252477303982147E+01   1   1   0   0
  -0.4759344611440753E+00   2   2   0   0
   0.7137758743754461E+00   0   0   0   0
-"""
+""")
 
-xacc.Initialize(['--n-electrons', '2'])
-op = vqe.compile(src)
-
+# Define PSO objective function
 def f(params):
+    # note 'no-mpi' at the compute energy, hamiltonian term 
+    # level, but we are using mpi at the higher PSO swarm level
     return vqe.execute(op,**{'mpi-provider':'no-mpi', 
         'task':'compute-energy', 
         'vqe-params':str(params[0])+','+str(params[1])}).energy
 
+# Construct Stochopy PSO algorithm and execute
 ea = Evolutionary(f, popsize=30, 
         lower=np.array([-np.pi,-np.pi]), 
         upper=np.array([np.pi, np.pi]), 
         n_dim=2, mpi=True)
 ea.optimize(solver="pso")
+
+# Print result
 if mpi_rank == 0:
     print('Our result = ', ea)
