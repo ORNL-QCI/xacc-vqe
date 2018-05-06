ba06e76b34e70e6515e5a9f64498a949ffd097c5
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-05-06 18:07:05 +0000

    adding a null rpath script to manylinux wheel build
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- tools/wheels/build_xacc_vqe_manylinux_wheels.sh
+++ tools/wheels/build_xacc_vqe_manylinux_wheels.sh
@@ -10,4 +10,5 @@ do
         export ver=`case $version in "cp36-cp36m") echo 3.6 ;; "cp35-cp35m") echo 3.5 ;; "cp34-cp34m") echo 3.4 ;; "cp33-cp33m") echo 3.3 ;; "cp27-cp27m") echo 2.7 ;; "cp27-cp27mu") echo 2.7 ;; *) echo "invalid";; esac`
         PYTHONPATH=/xacc/build/lib.linux-x86_64-$ver/xacc $myPython setup.py build -t tmp_build --executable="/usr/bin/env python"
 	PYTHONPATH=/xacc/build/lib.linux-x86_64-$ver/xacc $myPython setup.py bdist_wheel --skip-build
+        $rpath_python /xacc/tools/wheels/null_rpaths.py -v repair dist/xacc_vqe-0.1.0-$version-linux_x86_64.whl
 done
