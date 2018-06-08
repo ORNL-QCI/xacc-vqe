6c419b639955612dd165e33a9dcfdb42f690f1da
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-06-08 18:23:15 +0000

    incrementing version to 0.1.2
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class CMakeBuild(build_ext):
 
 s = setup(
     name='xacc-vqe',
-    version='0.1.1',
+    version='0.1.2',
     author='Alex McCaskey',
     author_email='xacc-dev@eclipse.org',
     packages=find_packages('python'),
