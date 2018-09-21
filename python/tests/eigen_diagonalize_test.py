f2252895992d7eaa9807400ce8cfd92ce726a385
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-09-21 13:10:17 -0400

    Update eigen_diagonalize_test.py

--- python/tests/eigen_diagonalize_test.py
+++ python/tests/eigen_diagonalize_test.py
@@ -43,4 +43,5 @@ class EigenDiagonalizeTest(unittest.TestCase):
         self.assertEqual(vqe.execute(op, buffer).energy, -1.137270422069755)
 
 if __name__ == '__main__':
-    unittest.main()
+    if xacc.hasAccelerator('tnqvm'):
+       unittest.main()     
