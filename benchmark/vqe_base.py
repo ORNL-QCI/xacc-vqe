e1981d9b77827715dc9b3c2cbed82d99aeb4ceaf
Zachary Parks <1zp@ornl.gov>
2018-11-29 11:47:11 -0500

    adding richardson extrap to vqe_base
    Signed-off-by: Zachary Parks <1zp@ornl.gov>

--- benchmark/vqe_base.py
+++ benchmark/vqe_base.py
@@ -4,6 +4,8 @@ import xaccvqe
 import ast
 import time
 import os
+import numpy as np 
+
 class VQEBase(Algorithm):
     
     def __init__(self):
@@ -86,3 +88,90 @@ class VQEBase(Algorithm):
                     f.write(str(exp)+',')
                 f.write(str(energy)+'\n')
             f.close()
+            
+        if 'richardson-extrapolation' in inputParams and inputParams['richardson-extrapolation']:
+            angles = buffer.getInformation('vqe-angles')
+            qpu = self.vqe_options_dict['accelerator']
+            self.vqe_options_dict['accelerator'] = xacc.getAcceleratorDecorator('rich-extrap',qpu)
+            self.vqe_options_dict['task'] = 'compute-energy'
+            xaccOp = self.op
+            self.vqe_options_dict['vqe-params'] = ','.join([str(x) for x in angles])
+
+            fileNames = {r:"%s_%s_%s_%s" % (os.path.splitext(buffer.getInformation('file-name'))[0],
+                            buffer.getInformation('accelerator'),
+                            'rich_extrap_'+str(r), timestr)+'.csv' for r in [1,3,5,7]}
+                            
+            nRE_Execs = 2 if not 'rich-extrap-iter' in inputParams else int(inputParams['rich-extrap-iter'])
+            if nRE_Execs < 2:
+                print('Richardson Extrapolation needs more than 1 execution. Setting to 2.')
+                nRE_execs = 2  
+            
+            for r in [1,3,5,7]:
+                f = open(fileNames[r], 'w')
+                xacc.setOption('rich-extrap-r',r)
+                
+                for i in range(nRE_Execs):
+                    richardson_buffer = qpu.createBuffer('q', self.n_qubits)
+                    results = xaccvqe.execute(xaccOp, richardson_buffer, **self.vqe_options_dict)
+                
+                    ps = richardson_buffer.getAllUnique('parameters')
+                    for p in ps:
+                        f.write(str(p).replace('[', '').replace(']', ''))
+                        energy = 0.0
+                        for c in richardson_buffer.getChildren('parameters', p):
+                            exp = c.getInformation('ro-fixed-exp-val-z') if c.hasExtraInfoKey('ro-fixed-exp-val-z') else c.getInformation('exp-val-z')
+                            energy += exp * c.getInformation('coefficient')
+                            f.write(','+str(exp))
+                        f.write(','+str(energy)+'\n')
+                f.close()
+
+            nParams = len(ps[0])
+            columns = ['t{}'.format(i) for i in range(nParams)]
+
+            kernelNames = [c.getInformation('kernel') for c in buffer.getChildren('parameters',ps[0])]
+            columns += kernelNames
+            columns.append('E')
+            
+            dat = [np.genfromtxt(fileNames[1], delimiter=',', names=columns),
+                np.genfromtxt(fileNames[3], delimiter=',', names=columns),
+                np.genfromtxt(fileNames[5], delimiter=',', names=columns),
+                np.genfromtxt(fileNames[7], delimiter=',', names=columns)]
+
+            allExps = [{k:[] for k in kernelNames} for i in range(4)]
+            allEnergies = []
+
+            temp = {r:[] for r in range(4)}
+
+            for i in range(nRE_Execs):
+                for r in range(4):
+                    for term in kernelNames:
+                        allExps[r][term].append(dat[r][term][i])
+                    temp[r].append(dat[r]['E'][i])
+
+            evars = [np.std(temp[r]) for r in range(4)]
+            xVals = [1,3,5,7]
+
+            avgExps = {k:[np.mean(allExps[r][k]) for r in range(4)] for k in kernelNames}
+            varExps = {k:[np.std(allExps[r][k]) for r in range(4)] for k in kernelNames}
+            energies = [np.mean(temp[r]) for r in range(4)]
+
+            def linear(x, a, b):
+                return a*x+b
+    
+            def exp(x, a,b):
+                return a*np.exp(b*x)# + b
+
+            from scipy.optimize import curve_fit
+            res = curve_fit(linear, xVals, energies, [1,energies[0]], sigma=evars)
+
+            print(res)
+            print('\nnoisy energy: ', energies[0])
+            print('\nrich linear extrap: ', res[0][1],'+- ', np.sqrt(np.diag(res[1])[1]))
+
+            res_exp = curve_fit(exp, xVals, energies, [0,0], sigma=evars)
+
+            print('\nrich exp extrap: ', exp(0,res_exp[0][0],res_exp[0][1]), '+-', np.sqrt(np.diag(res_exp[1])[1]))
+
+            print('\n')
+            print('E(r): ', energies)
+            print('a*exp(b*r):', [exp(x,res_exp[0][0],res_exp[0][1]) for x in xVals])
