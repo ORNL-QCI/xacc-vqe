25edfac8c37e240128d38ea4e36aa7be5dcd2902
Zachary Parks <1zp@ornl.gov>
2019-06-07 12:50:06 +0000

    changes to VQE decorator function to enable optimizer plugin functionality
    Signed-off-by: Zachary Parks <1zp@ornl.gov>

---
+++ python/decorators/scipy_optimizer.py
@@ -0,0 +1,32 @@
+from pelix.ipopo.decorators import (ComponentFactory, Property, Requires,
+                                    Provides, Instantiate)
+import xacc
+from xaccvqe import VQEOpt
+import xaccvqe as vqe
+import inspect
+from abc import abstractmethod, ABC
+from scipy.optimize import minimize
+
+@ComponentFactory("scipy_opt_factory")
+@Provides("vqe_optimization")
+@Property("_name", "name", "scipy-opt")
+@Property("_vqe_optimizer", "vqe_optimizer", "scipy-opt")
+@Instantiate("scipy_opt_instance")
+class ScipyOpt(VQEOpt):
+
+    def optimize(self, observable, buffer, optimizer_args, execParams):
+        super().optimize(observable, buffer, optimizer_args, execParams)
+
+        if 'vqe-params' in self.execParams:
+            init_args = [float(x) for x in self.execParams['vqe-params'].split(',')]
+        else:
+            import random
+            pi = 3.141592653
+            init_args = [random.uniform(-pi, pi) for _ in range(self.execParams['ansatz'].nParameters())]
+
+        opt_result = minimize(self.energy, init_args, **self.opt_args)
+
+    def energy(self, params):
+        super().energy(params)
+
+
--- python/decorators/wrappedVQE.py
+++ python/decorators/wrappedVQE.py
@@ -1,19 +1,35 @@
 from pelix.ipopo.decorators import (ComponentFactory, Property, Requires,
-                                    Provides, Instantiate)
+                                    Provides, Instantiate, BindField, UnbindField)
 import xacc
 import inspect
+import xaccvqe as vqe
 from _pyxaccvqe import *
 
 @ComponentFactory("wrapped_vqe_factory")
 @Provides("decorator_algorithm_service")
 @Property("_algorithm", "algorithm", "vqe")
 @Property("_name", "name", "vqe")
+@Requires("_vqe_optimizers", "vqe_optimization", aggregate=True)
 @Instantiate("wrapped_vqe_instance")
 class WrappedVQEF(xacc.DecoratorFunction):
 
+    def __init__(self):
+        self.vqe_optimizers = {}
+
+    @BindField("_vqe_optimizers")
+    def bind_optimizers(self, field, service, svc_ref):
+        if svc_ref.get_property('vqe_optimizer'):
+            optimizer = svc_ref.get_property('vqe_optimizer')
+            self.vqe_optimizers[optimizer] = service
+
+    @UnbindField("_vqe_optimizers")
+    def unbind_optimizers(self, field, service, svc_ref):
+        if svc_ref.get_property('vqe_optimizer'):
+            optimizer = svc_ref.get_property('vqe_optimizer')
+            del vqe_optimizers[optimizer]
+
     def __call__(self, *args, **kwargs):
         super().__call__(*args, **kwargs)
-
         def getParams(params): return ','.join(map(str, params))
         execParams = {'accelerator': self.qpu, 'ansatz': self.compiledKernel, 'task': 'vqe'}
         obs = self.kwargs['observable']
@@ -30,36 +46,85 @@ class WrappedVQEF(xacc.DecoratorFunction):
             execParams['vqe-params'] = arStr
         if 'optimizer' in self.kwargs:
             optimizer = self.kwargs['optimizer']
+            optimizer_args = {}
+            # we want to use scipy
             if 'scipy-' in optimizer:
-                optimizer = optimizer.replace('scipy-', '')
-                from scipy.optimize import minimize
                 execParams['task'] = 'compute-energy'
-
-                energies = []
-                def energy(params):
-                    pStr = getParams(params)
-                    execParams['vqe-params'] = pStr
-                    e = execute(obs, buffer, **execParams).energy
-                    energies.append(e)
-                    return e
-                if len(ars) == 0:
-                    import random
-                    pi = 3.141592653
-                    ars = [
-                        random.uniform(-pi, pi) for _ in range(self.compiledKernel.nParameters())]
-                optargs = {'method': optimizer, 'options': {'disp': True}}
+                # get the scipy-opt optimizer plugin
+                opt_instance = self.vqe_optimizers['scipy-opt']
+                optimizer = optimizer.replace('scipy-', '')
+                # set the scipy-optimizer to the specified method
+                optimizer_args["method"] = optimizer
                 if 'options' in self.kwargs:
-                    optargs['options'] = self.kwargs['options']
-                if 'opt_params' in self.kwargs:
-                    for k, v in self.kwargs['opt_params'].items():
-                        optargs[k] = v
-                opt_result = minimize(energy, ars, **optargs)
-                buffer.addExtraInfo('vqe-energies',energies)
+                    optimizer_args['options'] = self.kwargs['options']
+                # call the optimizer-plugin optimize() method
+                # with the optimizer settings and VQE settings
+                opt_instance.optimize(obs, buffer, optimizer_args, execParams)
+
+                # now we don't need any of this
+
+                # from scipy.optimize import minimize
+                # energies = []
+                # def energy(params):
+                #     pStr = getParams(params)
+                #     execParams['vqe-params'] = pStr
+                #     e = vqe.execute(obs, buffer, **execParams).energy
+                #     energies.append(e)
+                #     return e
+                # if len(ars) == 0:
+                #     import random
+                #     pi = 3.141592653
+                #     ars = [
+                #         random.uniform(-pi, pi) for _ in range(self.compiledKernel.nParameters())]
+                # optargs = {'method': optimizer, 'options': {'disp': True}}
+                # if 'options' in self.kwargs:
+                #     optargs['options'] = self.kwargs['options']
+                # if 'tol' in optargs['options']:
+                #     print(optargs['options']['tol'])
+                # if 'opt_params' in self.kwargs:
+                #     for k, v in self.kwargs['opt_params'].items():
+                #         optargs[k] = v
+
+                # opt_result = minimize(energy, ars, **optargs)
+                # print(optargs)
+                # print(opt_result)
+
+                # the optimizer has all of the energies now
+                buffer.addExtraInfo('vqe-energies',opt_instance.energies)
                 return
+            # until we have a standard naming for these too
+            # use bobyqa instead
+            elif 'bobyqa' or 'BOBYQA' in optimizer:
+                execParams['task'] = 'compute-energy'
+                opt_instance = self.vqe_optimizers['bobyqa-opt']
+                if 'options' in self.kwargs:
+                    optimizer_args = self.kwargs['options']
+                opt_instance.optimize(obs, buffer, optimizer_args, execParams)
+
+                # we dont need any of this anymore
+
+                # import pybobyqa
+                # energies = []
+                # def energy(params):
+                #     pStr = getParams(params)
+                #     execParams['vqe-params'] = pStr
+                #     e = vqe.execute(obs, buffer, **execParams).energy
+                #     return e
+                # if len(ars) == 0:
+                #     import random
+                #     pi = 3.141592653
+                #     ars = np.array([
+                #         random.uniform(-pi, pi) for _ in range(self.compiledKernel.nParameters())])
+                # print(type(ars))
+                # print(ars)
+                # opt_result = pybobyqa.solve(energy, ars)
+                # print(opt_result)
+
+                buffer.addExtraInfo('vqe-energies', opt_instance.energies)
             else:
                 xacc.setOption('vqe-backend', optimizer)
                 if 'opt_params' in self.kwargs:
                     for k, v in self.kwargs['opt_params'].items():
                         xacc.setOption(k, str(v))
-        execute(obs, buffer, **execParams)
+        vqe.execute(obs, buffer, **execParams)
         return
